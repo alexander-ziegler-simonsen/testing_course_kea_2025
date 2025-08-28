043a19533c8325b509589b118be5e411624e60fa
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-08-28 21:51:33 +0200

    lesson01 - python - almost done - passed 13 out of 15 tests

--- Lesson01/python/rom_num_converter.py
+++ Lesson01/python/rom_num_converter.py
@@ -8,4 +8,92 @@ max_value = "MMMCMXCIX"                     # max value 'MMMCMXCIX' = 3999
 
 
 def roman_num_converter(roman_value):
-    return 404
+    # first check if something is wrong
+
+    # too many repeats
+    for i in non_repeat:
+        if(roman_value.count(i) > 1):
+            return "Too many " + i
+    
+    # contain unkown chars
+    for char in roman_value:
+        if(not char in roman_numerals):
+            return "unkown input letters"
+        
+    # more than 3 sequentially repeats
+    counter = 0
+    lastChar = ""
+    handlingOneOfTheLetters = False
+    for i in range(len(roman_value)):
+        #print(str(i) + " | counter : " + str(counter) + " | lastchar : " + lastChar + " | handlingOneOfTheLetters : " + str(handlingOneOfTheLetters))
+        
+        currentChar = roman_value[i]
+        # keep finding more
+        if(currentChar in max_3_sequentially and handlingOneOfTheLetters):
+            if(lastChar != currentChar):
+                counter = 1
+                lastChar = currentChar
+            else:
+                counter += 1
+                lastChar = currentChar
+        # found one 
+        if(currentChar in max_3_sequentially and not handlingOneOfTheLetters):
+            lastChar = currentChar
+            handlingOneOfTheLetters = True
+            counter += 1
+        # didn't find one
+        if(currentChar not in max_3_sequentially):
+            lastChar = currentChar
+            counter = 0
+            handlingOneOfTheLetters = False
+        # if we are over 3
+        if(counter > 3):
+            return "the count of I,X,C or M went over 3"
+    
+    output = 0
+    previousValue = 0
+    # the main logic loop
+    for i in range(len(roman_value)):
+
+        # set the values
+        currentNum = roman_numerals[roman_value[i]]
+        nextI = i+1
+        nextII = i+2
+        nextNum = 0
+        next2Num = 0
+        if(nextI < len(roman_value)):
+            nextNum = roman_numerals[roman_value[(nextI)]]
+        if(nextII < len(roman_value)):
+            next2Num = roman_numerals[roman_value[(nextII)]]
+
+        # first time runing 
+        if(previousValue == 0):
+            # check if the first part is negative or positive
+            if(roman_numerals[roman_value[(nextI)]] > currentNum):
+                output -= currentNum
+            else:
+                output += currentNum
+
+        else:
+
+            # if it is the same, add it
+            if(currentNum == previousValue):
+                output += currentNum
+            # if current is bigger than the next, add it
+            elif(currentNum > nextNum and next2Num != 0):
+                output += currentNum
+            elif(currentNum < nextNum and next2Num != 0):
+                output -= currentNum
+        
+        # overwrite the previousValue value
+        previousValue = currentNum
+
+    
+    # check if the value is bigger than Max value
+    if(output > 3990):
+        return "over max value"
+
+    return str(output)
+
+# output = roman_num_converter("VIIIIDD")
+# print(output)
--- Lesson01/python/test_rom_num_converter.py
+++ Lesson01/python/test_rom_num_converter.py
@@ -2,17 +2,15 @@ from rom_num_converter import roman_num_converter
 
 # tests needed for this function
 ## check all the rules are right
-### (not written yet) check what will happen if, you have more than one set of add/sub in the same string
-### (not written yet) check what will happen if, the order of add/sub is wrong
 ## check edge cases
 ### check what happens when you add a letter, that is not on the list
 ### (not written yet) check what will happen if, we add a input that is not a string (like bool, int or float)
 
 def test_converter_with_1867():
-    assert roman_num_converter("MDCCCLXVII") == 1867, "should be 1867"
+    assert roman_num_converter("MDCCCLXVII") == "1867", "should be 1867"
 
 def test_convert_with_substract_94():
-    assert roman_num_converter("XCIV") == 94, "should be 94"
+    assert roman_num_converter("XCIV") == "94", "should be 94"
 
 
 # catches 
@@ -20,16 +18,25 @@ def test_convert_catch_over_max():
     assert roman_num_converter("MMMDCMXCIX") == "over max value", "should be 'over max value'"
 
 def test_convert_catch_wrong_letter():
-    assert roman_num_converter("XVIYTREQAPU") == "wrong input", "should be 'wrong input'"
+    assert roman_num_converter("XVIYTREQAPU") == "unkown input letters", "should be 'unkown input letters'"
+
+def test_convert_catch_repeated_V():
+    assert roman_num_converter("VVII") == "Too many V", "should be 'Too many V'"
+
+def test_convert_catch_repeated_D():
+    assert roman_num_converter("DDII") == "Too many D", "should be 'Too many D'"
+
+def test_convert_catch_repeated_L():
+    assert roman_num_converter("LLII") == "Too many L", "should be 'Too many L'"
 
 def test_convert_catch_4I():
-    assert roman_num_converter("VIIII") == "I count went over 3", "should be 'I count went over 3'"
+    assert roman_num_converter("VIIII") == "the count of I,X,C or M went over 3", "should be 'the count of I,X,C or M went over 3'"
 
 def test_convert_catch_4X():
-    assert roman_num_converter("MXXXX") == "X count went over 3", "should be 'X count went over 3'"
+    assert roman_num_converter("MXXXX") == "the count of I,X,C or M went over 3", "should be 'the count of I,X,C or M went over 3'"
 
 def test_convert_catch_4C():
-    assert roman_num_converter("MCCCC") == "C count went over 3", "should be 'C count went over 3'"
+    assert roman_num_converter("MCCCC") == "the count of I,X,C or M went over 3", "should be 'the count of I,X,C or M went over 3'"
 
 def test_convert_catch_4M():
-    assert roman_num_converter("MMMMI") == "M count went over 3", "should be 'M count went over 3'"
+    assert roman_num_converter("MMMMI") == "the count of I,X,C or M went over 3", "should be 'the count of I,X,C or M went over 3'"
