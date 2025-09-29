6e548823d558976057747a6ef1338a895f504ddb
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-09-30 00:13:53 +0200

    lesson05 - end for now

---
+++ lesson05/measureConverter/python/conversions/grades.py
@@ -0,0 +1,13 @@
+
+from enum import Enum
+
+class gradeType(Enum):
+    DANISH = 'Danish'
+    USA = 'American'
+
+class gradeClass:
+    # def __init__(self):
+
+    
+    def convert(self):
+        print("too late")
